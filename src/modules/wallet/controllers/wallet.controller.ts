e72b5f20c9a7e8b5c6ec1afd091fca6cc3d08180
Giselle <giselle.costa1409@hotmail.com>
2026-02-13 13:45:58 -0300

    update wallet controller

--- src/modules/wallet/controllers/wallet.controller.ts
+++ src/modules/wallet/controllers/wallet.controller.ts
@@ -1,23 +1,37 @@
 import { Request, Response } from 'express';
 import { IWalletService } from '../services/wallet.service.interface';
+import { 
+    CreateWalletBody, 
+    WalletIdParams,  
+} from '../types/wallet.types';
+
+import { 
+    createWalletSchema,
+    walletIdParamSchema,
+ } from '../schemas/wallet.schemas';
 
 export class WalletController {
     constructor(private walletService: IWalletService) {}
 
-    async createWallet(req: Request, res: Response) {
+    async createWallet(req: Request<{}, {}, CreateWalletBody>, res: Response) {
         try {
-            const { name, cpf, password } = req.body;
-            const wallet = await this.walletService.createWallet(name, cpf, password);
+            const data = createWalletSchema.parse(req.body);
+            const wallet = await this.walletService.createWallet(
+                data.name, 
+                data.cpf, 
+                data.password
+            );
             return res.status(201).json(wallet);
         } catch (error: any) {
             return res.status(400).json({ error: error.message });
         }
     }
 
-    async getWalletById(req: Request, res: Response) {
+    async getWalletById(req: Request<WalletIdParams>, res: Response) {
         try {
-            const { id } = req.params;
-            const wallet = await this.walletService.getWalletById(id);
+            const data = walletIdParamSchema.parse(req.params);
+            const wallet = await this.walletService.getWalletById(data.id);
+
             if (!wallet) {
                 return res.status(404).json({ error: 'Wallet not found' });
             }
