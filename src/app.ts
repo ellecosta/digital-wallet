f549ceb9086e9aac15244ea205f7cce3b745e93d
Giselle <giselle.costa1409@hotmail.com>
2026-03-09 16:20:16 -0300

    app file update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import walletRoutes from "./modules/wallet/routes/wallet.routes";
 import transactionRoutes from "./modules/wallet/routes/transaction.routes";
+import complianceTransactionRoutes from "./modules/wallet/routes/compliance-transactions.routes"
+import statementRoutes from "./modules/wallet/routes/statement.routes"
 
 const app = express();
 
@@ -9,5 +11,7 @@ app.use(express.json());
 // routes
 app.use("/wallets", walletRoutes);
 app.use("/transactions", transactionRoutes);
+app.use("/compliance", complianceTransactionRoutes);
+app.use("/statement", statementRoutes);
 
 export default app;
